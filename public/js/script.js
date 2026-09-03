6c0b8eecec0be68c507d8a54d585815d981ba671
SatriaFarel <satriafarel40@gmail.com>
2026-09-03 18:57:16 +0700

    perbaikan api url

--- public/js/script.js
+++ public/js/script.js
@@ -10,7 +10,7 @@
         setInterval(updateDateTime, 1000);
         updateDateTime();
 
-        const API_URL = 'http://todo-app-sf/api/tasks';
+        const API_URL = 'https://todo-app-sf/api/tasks';
 
         // Mengambil seluruh data tugas dari backend
         async function fetchTasks() {
